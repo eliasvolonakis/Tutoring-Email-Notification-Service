96a864465c424d1117c50386cab6ee0259762c44
Elias Volonakis <eliasvolonakis@Eliass-MacBook-Pro.local>
2023-03-09 10:44:18 -0500

    Added basic send_email function

--- send_email.py
+++ send_email.py
@@ -5,6 +5,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
+# Set Up All Environment Variables
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
@@ -20,25 +21,29 @@ print("EMAIL_PASSWORD" + str(email_password))
 print("PORT" + str(port))
 print("EMAIL_SERVER" + srt(email_server))
 
+def sendmail(student_name, session_time, student_email, zoom_link)
+    em = EmailMessage()
+    em['From'] = email_sender
+    em['To'] = student_email
+    em['Subject'] = "Session Reminder {session_time}".format(session_time=session_time)
 
-# email_sender = "envolonakis@gmail.com"
-# email_password = "nffygmqgllrtwfew"
-# email_receiver = "envolonakis@gmail.com"
+    body = """
+    Good morning {studen_name}!
 
-# subject = "Test Subject"
-# body = "Test Body"
+    I'll see you this evening from {session_time} for our tutoring session. 
+    Please use the zoom link below:
+    {zoom_link}}
 
-# em = EmailMessage()
-# em['From'] = email_sender
-# em['To'] = email_receiver
-# em['Subject'] = subject
-# em.set_content(body)
+    Thanks, 
+    Elias
+    """
 
-# context = ssl.create_default_context()
+    em.set_content(body)
+    context = ssl.create_default_context()
 
-# email_server = 'smtp.gmail.com'
-# port = 465
-# with smtplib.SMTP_SSL(email_server, port, context = context) as smtp:
-#     smtp.login(email_sender, email_password)
-#     smtp.sendmail(email_sender, email_receiver, em.as_string())
+    email_server = 'smtp.gmail.com'
+    port = 465
+    with smtplib.SMTP_SSL(email_server, port, context = context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
 
